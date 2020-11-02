cde4461eece1e253c0859d575ca1e45f0e28f5ca
MikeZed <39437367+MikeZed@users.noreply.github.com>
2020-11-02 23:24:26 +0200

    file organization

--- ex0/CW_Ex0/answer.c
+++
@@ -1,9 +0,0 @@
-
-#include "answer.h"
-
-struct Answer WhatIsTheAnswer()
-{
-	struct Answer TheAnswer;
-	TheAnswer.answer = 42.0;
-	return TheAnswer;
-}
--- ex0/CW_Ex0/main.c
+++
@@ -1,9 +0,0 @@
-
-//#include <stdio.h>
-//#include "answer.h"
-#include "PrintTheAnswer.h"
-
-void main()
-{
-	PrintTheAnswer();
-}
