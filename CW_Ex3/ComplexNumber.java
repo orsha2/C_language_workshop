aafc073cee6bc78ff09588862967eac2611b4685
MikeZed <39437367+MikeZed@users.noreply.github.com>
2020-12-14 19:11:13 +0200

    final solution

--- CW_Ex3/ComplexNumber.java
+++ CW_Ex3/ComplexNumber.java
@@ -1,63 +1,76 @@
 import java.lang.Math;
 
-public class ComplexNumber{
+public class ComplexNumber {
     private static final double EPSILON = 0.001;
-
     private final double realPart;
     private final double imaginaryPart;
 
-    public ComplexNumber(double real, double imginary){
+    public ComplexNumber(double real, double imaginary) {
         this.realPart = real;
-        this.imaginaryPart = imginary;  
+        this.imaginaryPart = imaginary;
     }
 
-    public ComplexNumber plus(ComplexNumber other){
-        double real = this.realPart + other.realPart;
-        double imginary = this.imaginaryPart + other.imaginaryPart;
-        return new ComplexNumber ( real ,imginary); 
-    }
-
-    public ComplexNumber minus(ComplexNumber other){
-        double real = this.realPart - other.realPart;
-        double imginary = this.imaginaryPart - other.imaginaryPart;
-        return new ComplexNumber ( real ,imginary); 
-    }
-
-    public ComplexNumber times(ComplexNumber other){
-        double real = this.realPart * other.realPart - this.imaginaryPart * other.imaginaryPart;
-        double imginary = this.realPart * other.imaginaryPart + this.imaginaryPart * other.realPart;
-        return new ComplexNumber ( real ,imginary); 
-        
-    }
-
-    private ComplexNumber inverse() {
-        double radius = this.getRadius();
-        return new ComplexNumber(this.realPart / Math.pow(radius,2) ,- this.imaginaryPart / Math.pow(radius,2) );
-    }
-
-    public ComplexNumber divide(ComplexNumber other){
-        return this.times(other.inverse());
-    }    
-
-    public double getRealPart(){
+    public double getRealPart() {
         return this.realPart;
     }
 
-    public double getImaginaryPart(){
+    public double getImaginaryPart() {
         return this.imaginaryPart;
     }
 
-    public double getRadius (){
-        return Math.sqrt( Math.pow(this.realPart,2) + Math.pow(this.imaginaryPart,2) );
+    public double getRadius() {
+        return this.getDistance(0, 0);
     }
 
-    public double getArgument (){
+    public double getArgument() {
         return Math.atan2(this.imaginaryPart, this.realPart);
     }
 
-    public boolean almostEquals(ComplexNumber other ){
-        return this.minus(other).getRadius() < EPSILON;
+    public ComplexNumber plus(ComplexNumber other) {
+        double real = this.realPart + other.realPart;
+        double imginary = this.imaginaryPart + other.imaginaryPart;
+
+        return new ComplexNumber(real, imginary);
     }
 
+    public ComplexNumber minus(ComplexNumber other) {
+        double real = this.realPart - other.realPart;
+        double imginary = this.imaginaryPart - other.imaginaryPart;
 
+        return new ComplexNumber(real, imginary);
+    }
+
+    public ComplexNumber times(ComplexNumber other) {
+        double real = this.realPart * other.realPart - this.imaginaryPart * other.imaginaryPart;
+        double imginary = this.realPart * other.imaginaryPart + this.imaginaryPart * other.realPart;
+
+        return new ComplexNumber(real, imginary);
+    }
+
+    public ComplexNumber divide(ComplexNumber other) {
+        return this.times(other.getInverse());
+    }
+
+    public boolean almostEquals(ComplexNumber other) {
+        return (this.getDistance(other.realPart, other.imaginaryPart) < EPSILON);
+    }
+
+    static public ComplexNumber fromPolarCoordinates(double radius, double argument) {
+        double real = radius * Math.cos(argument);
+        double imaginary = radius * Math.sin(argument);
+
+        return new ComplexNumber(real, imaginary);
+    }
+
+    private ComplexNumber getInverse() {
+        return ComplexNumber.fromPolarCoordinates(1 /  this.getRadius(), -this.getArgument());
+    }
+
+    private double getDistance(double real, double imaginary) {
+        double realPartDiff = this.realPart - real;
+        double imaginaryPartDiff = this.imaginaryPart - imaginary;
+
+        double distance = Math.sqrt(realPartDiff * realPartDiff + imaginaryPartDiff * imaginaryPartDiff);
+        return distance;
+    }
 }
